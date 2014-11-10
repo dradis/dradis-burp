4b86c8ce989ddcc0d237447fde4ab87e7aa3767d
Jose A. Rilla <jose@rilla.es>
2014-11-10 09:21:28 +0100

    Fix dependencies, update base hooks, tidy up a bit

--- lib/dradis-burp.rb
+++ lib/dradis-burp.rb
@@ -1,1 +1,5 @@
+# Hook to the framework base clases
+require 'dradis-plugins'
+
+# Load this add-on's engine
 require 'dradis/plugins/burp'
--- lib/dradis/plugins/burp.rb
+++ lib/dradis/plugins/burp.rb
@@ -1,11 +1,18 @@
-require 'dradis/plugins/burp/engine'
-require 'dradis/plugins/burp/filters'
-require 'dradis/plugins/burp/parser'
-require 'dradis/plugins/burp/version'
-
 module Dradis
   module Plugins
     module Burp
+      # This is required while we transition the Upload Manager to use
+      # Dradis::Plugins
+      module Meta
+        NAME = "Burp Scanner output (.xml) file upload"
+        EXPECTS = "Burp Scanner XML output. Go to the Scanner tab > right-click item > generate report"
+        module VERSION
+          include Dradis::Plugins::Burp::VERSION
+        end
+      end
     end
   end
 end
+
+require 'dradis/plugins/burp/engine'
+require 'dradis/plugins/burp/version'
