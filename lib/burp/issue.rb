042a3b646b71ae5f3e666d7b6421d25b999b11f5
Jose A. Rilla <jose@rilla.es>
2014-11-30 17:51:56 +0100

    Add responds_to? to Burp::Issue

--- lib/burp/issue.rb
+++ lib/burp/issue.rb
@@ -29,6 +29,13 @@ module Burp
       ]
     end
 
+    # This allows external callers (and specs) to check for implemented
+    # properties
+    def respond_to?(method, include_private=false)
+      return true if supported_tags.include?(method.to_sym)
+      super
+    end
+
     # This method is invoked by Ruby when a method that is not defined in this
     # instance is called.
     #
