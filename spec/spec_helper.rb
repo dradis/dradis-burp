b3617e911a83a1397879990107dbe90598791f6c
George Millo <georgejulianmillo@gmail.com>
2015-04-20 18:45:05 +0800

    set RSpec default in .rspec instead of spec_helper.
    This way they can be overridden at the command line, e.g.: 'rspec -f progress'

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,8 +6,4 @@ require 'combustion'
 Combustion.initialize!
 
 RSpec.configure do |config|
-  # Enable colors
-  config.color = true
-  # Use the specified formatter
-  config.formatter = :documentation
 end
