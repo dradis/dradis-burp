b6f7224809eeadb56c46e08d30c255e5cb24c5d1
Daniel Martin <etd-github@nomejortu.com>
2016-03-14 12:45:51 +0100

    We no longer need a Meta module

--- lib/dradis/plugins/burp.rb
+++ lib/dradis/plugins/burp.rb
@@ -9,21 +9,3 @@ require 'dradis/plugins/burp/engine'
 require 'dradis/plugins/burp/field_processor'
 require 'dradis/plugins/burp/importer'
 require 'dradis/plugins/burp/version'
-
-# This is required while we transition the Upload Manager to use
-# Dradis::Plugins only
-module Dradis
-  module Plugins
-    module Burp
-      # This is required while we transition the Upload Manager to use
-      # Dradis::Plugins
-      module Meta
-        NAME = "Burp Scanner output (.xml) file upload"
-        EXPECTS = "Burp Scanner XML output. Go to the Scanner tab > right-click item > generate report"
-        module VERSION
-          include Dradis::Plugins::Burp::VERSION
-        end
-      end
-    end
-  end
-end
