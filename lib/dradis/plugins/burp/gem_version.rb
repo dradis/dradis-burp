b1308a9b87e8a008da3692b92fa66cef5f7fb14a
Daniel Martin <etd-github@nomejortu.com>
2017-02-22 14:41:16 +0100

    Detect non-base64 encoded files and binary req/res data

--- lib/dradis/plugins/burp/gem_version.rb
+++ lib/dradis/plugins/burp/gem_version.rb
@@ -9,7 +9,7 @@ module Dradis
       module VERSION
         MAJOR = 3
         MINOR = 0
-        TINY = 1
+        TINY = 2
         PRE = nil
 
         STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
