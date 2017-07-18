806e913bdeb348276e665de931fe494066cb1488
Daniel Martin <etd-github@nomejortu.com>
2017-07-18 17:21:17 +0200

    Thor - use Framework's logger

--- lib/tasks/thorfile.rb
+++ lib/tasks/thorfile.rb
@@ -7,9 +7,6 @@ class BurpTasks < Thor
   def upload(file_path)
     require 'config/environment'
 
-    logger = Logger.new(STDOUT)
-    logger.level = Logger::DEBUG
-
     unless File.exists?(file_path)
       $stderr.puts "** the file [#{file_path}] does not exist"
       exit -1
@@ -19,8 +16,6 @@ class BurpTasks < Thor
 
     importer = Dradis::Plugins::Burp::Importer.new(logger: logger)
     importer.import(file: file_path)
-
-    logger.close
   end
 
 end
