848ecbb792393cfce0274cde23a8d073a0532faf
Xavi Vila <xavivm@gmail.com>
2017-12-21 10:06:00 +0000

    bump version + update CHANGELOG

--- lib/dradis/plugins/burp/gem_version.rb
+++ lib/dradis/plugins/burp/gem_version.rb
@@ -8,8 +8,8 @@ module Dradis
 
       module VERSION
         MAJOR = 3
-        MINOR = 8
-        TINY = 1
+        MINOR = 9
+        TINY = 0
         PRE = nil
 
         STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
