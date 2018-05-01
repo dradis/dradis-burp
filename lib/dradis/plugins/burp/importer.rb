83982c27d29812afd9e6febb829ae757df215a5b
Rachael Korinek <rachael.korinek@gmail.com>
2018-05-01 17:52:23 -0500

    Update plugin logic so that extension findings are imported separately

--- lib/dradis/plugins/burp/importer.rb
+++ lib/dradis/plugins/burp/importer.rb
@@ -28,67 +28,100 @@ module Dradis::Plugins::Burp
       end
 
       # This will be filled in by the Processor while iterating over the issues
-      hosts         = []
-      affected_host = nil
-      issue_text    = nil
-      evidence_text = nil
+      @hosts         = []
+      @affected_host = nil
+      @issue_text    = nil
+      @evidence_text = nil
 
       doc.xpath('issues/issue').each do |xml_issue|
-        issue_name = xml_issue.at('name').text
-        issue_type = xml_issue.at('type').text.to_i
-
-        logger.info{ "Adding #{ issue_name } (#{ issue_type })" }
-
-        host_label = xml_issue.at('host')['ip']
-        host_label = xml_issue.at('host').text if host_label.empty?
-        affected_host = content_service.create_node(label: host_label, type: :host)
-        logger.info{ "\taffects: #{ host_label }" }
-
-        if !hosts.include?(affected_host.label)
-          hosts << affected_host.label
-          url = xml_issue.at('host').text
-          affected_host.set_property(:hostname, url)
-          affected_host.save
-        end
-
-        issue_text = template_service.process_template(
-          template: 'issue',
-          data: xml_issue)
-
-        if issue_text.include?(::Burp::INVALID_UTF_REPLACE)
-          logger.info %{
-            "\tdetected invalid UTF-8 bytes in your issue. " \
-            "Replacing them with '#{::Burp::INVALID_UTF_REPLACE}'."
-          }
-        end
-
-        issue = content_service.create_issue(
-          text: issue_text,
-          id: issue_type)
-
-        logger.info{ "\tadding evidence for this instance to #{ affected_host.label }."}
-
-        evidence_text = template_service.process_template(
-          template: 'evidence',
-          data: xml_issue
-        )
-
-        if evidence_text.include?(::Burp::INVALID_UTF_REPLACE)
-          logger.info {
-            "\tdetected invalid UTF-8 bytes in your evidence. " \
-            "Replacing them with '#{::Burp::INVALID_UTF_REPLACE}'."
-          }
-        end
-
-        content_service.create_evidence(
-          issue: issue,
-          node: affected_host,
-          content: evidence_text
-        )
-
+        process_report_host(xml_issue)
       end
+
       logger.info{ 'Burp Scanner results successfully imported' }
       return true
+
+    end
+
+    private
+    def process_report_host(xml_issue)
+      host_label = xml_issue.at('host')['ip']
+      host_label = xml_issue.at('host').text if host_label.empty?
+      affected_host = content_service.create_node(label: host_label, type: :host)
+      logger.info{ "\taffects: #{ host_label }" }
+
+      if !@hosts.include?(affected_host.label)
+        @hosts << affected_host.label
+        url = xml_issue.at('host').text
+        affected_host.set_property(:hostname, url)
+        affected_host.save
+      end
+
+      if xml_issue.at('type').text.to_str == "134217728"
+        process_extension_issues(affected_host, xml_issue)
+      else
+        process_burp_issues(affected_host, xml_issue)
+      end
+    end
+    
+    def process_burp_issues(affected_host, xml_issue)
+      issue_name = xml_issue.at('name').text
+      issue_type = xml_issue.at('type').text.to_i
+
+      logger.info{ "Adding #{ issue_name } (#{ issue_type })" }
+      issue_text = template_service.process_template(template: 'issue', data: xml_issue)
+
+      if issue_text.include?(::Burp::INVALID_UTF_REPLACE)
+        logger.info %{
+          "\tdetected invalid UTF-8 bytes in your issue. " \
+          "Replacing them with '#{::Burp::INVALID_UTF_REPLACE}'."
+        }
+      end
+
+      issue = content_service.create_issue(text: issue_text, id: issue_type)
+
+      logger.info{ "\tadding evidence for this instance to #{ affected_host.label }."}
+
+      evidence_text = template_service.process_template(template: 'evidence', data: xml_issue)
+
+      if evidence_text.include?(::Burp::INVALID_UTF_REPLACE)
+        logger.info {
+          "\tdetected invalid UTF-8 bytes in your evidence. " \
+          "Replacing them with '#{::Burp::INVALID_UTF_REPLACE}'."
+        }
+      end
+
+      content_service.create_evidence(issue: issue, node: affected_host, content: evidence_text)
+    end
+
+    def process_extension_issues(affected_host, xml_issue)
+      ext_name = xml_issue.at('name').text
+      ext_name = ext_name.gsub!(" ", "")
+
+      logger.info{ "Adding #{ ext_name })" }
+
+      issue_text = template_service.process_template(template: 'issue', data: xml_issue)
+
+      if issue_text.include?(::Burp::INVALID_UTF_REPLACE)
+        logger.info %{
+          "\tdetected invalid UTF-8 bytes in your issue. " \
+          "Replacing them with '#{::Burp::INVALID_UTF_REPLACE}'."
+        }
+      end
+
+      issue = content_service.create_issue(text: issue_text, id: ext_name)
+
+      logger.info{ "\tadding evidence for this instance to #{ affected_host.label }."}
+
+      evidence_text = template_service.process_template(template: 'evidence', data: xml_issue)
+
+      if evidence_text.include?(::Burp::INVALID_UTF_REPLACE)
+        logger.info {
+          "\tdetected invalid UTF-8 bytes in your evidence. " \
+          "Replacing them with '#{::Burp::INVALID_UTF_REPLACE}'."
+        }
+      end
+
+      content_service.create_evidence(issue: issue, node: affected_host, content: evidence_text)
     end
 
   end
