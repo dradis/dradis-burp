5435cd36c5a8712f9f8985156c500ef77a74af26
Daniel Martin <53006+etdsoft@users.noreply.github.com>
2021-01-29 16:30:09 +0100

    Improve syntax to please Rails 6's loader

--- lib/dradis/plugins/burp/engine.rb
+++ lib/dradis/plugins/burp/engine.rb
@@ -1,25 +1,21 @@
-module Dradis
-  module Plugins
-    module Burp
-      class Engine < ::Rails::Engine
-        isolate_namespace Dradis::Plugins::Burp
+module Dradis::Plugins::Burp
+  class Engine < ::Rails::Engine
+    isolate_namespace Dradis::Plugins::Burp
 
-        include ::Dradis::Plugins::Base
-        description 'Processes Burp Scanner output'
-        provides :upload
+    include ::Dradis::Plugins::Base
+    description 'Processes Burp Scanner output'
+    provides :upload
 
-        # Because this plugin provides two export modules, we have to overwrite
-        # the default .uploaders() method.
-        #
-        # See:
-        #  Dradis::Plugins::Upload::Base in dradis-plugins
-        def self.uploaders
-          [
-            Dradis::Plugins::Burp::Html,
-            Dradis::Plugins::Burp::Xml
-          ]
-        end
-      end
+    # Because this plugin provides two export modules, we have to overwrite
+    # the default .uploaders() method.
+    #
+    # See:
+    #  Dradis::Plugins::Upload::Base in dradis-plugins
+    def self.uploaders
+      [
+        Dradis::Plugins::Burp::Html,
+        Dradis::Plugins::Burp::Xml
+      ]
     end
   end
 end
